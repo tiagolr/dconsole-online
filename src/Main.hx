4e9ecc74762db35792bb7d68aa76447ddaf4eb1a
TiagoLr <prog4mr@gmail.com>
2014-10-17 01:26:10 +0100

    First commit, console and code editor working.

---
+++ src/Main.hx
@@ -0,0 +1,40 @@
+package ;
+
+import haxe.PosInfos;
+import js.Lib;
+import pgr.dconsole.DC;
+import pgr.dconsole.DConsole;
+import pgr.dconsole.ui.DCEmtpyInterface;
+
+/**
+ * ...
+ * @author TiagoLr
+ */
+@:expose
+class Main {
+	
+	static function main() {
+		haxe.Log.trace = function(v : Dynamic, ?inf : PosInfos) { DC.log(v); }
+	}
+	
+	public static function init() {
+		DC.init();
+		DC.eval('help');
+		
+		/*var cmds = DC.instance.commands.commandsMap;
+		
+		if (cmds.exists('monitor')) {
+			cmds.remove('monitor');
+		}
+		
+		if (cmds.exists('profiler')) {
+			cmds.remove('profiler');
+		}
+		
+		DC.registerFunction(runCode, "run", "Runs the code inside the text editor");*/
+	}
+	
+	static private function runCode() {
+		//Lib.eval('document.dispatchEvent(new Event("console_run"));');
+	}
+}
